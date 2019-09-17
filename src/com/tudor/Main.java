f9beb9c6aa9710b88155d49f7e2b9a9f90bff994
tudorcgb <cabautudor@gmail.com>
2019-09-17 23:03:19 +0300

    resubmit

--- src/com/tudor/Main.java
+++ src/com/tudor/Main.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+    import static org.junit.Assert.assertEquals;
 
 public class Main {
 
@@ -17,13 +17,39 @@ public class Main {
 
         countryCodes.put("GB",44);
         countryCodes.put("US",1);
+        countryCodes.put("FR", 33);
+        countryCodes.put("CHN", 86);
+
         prefixes.put("GB","0");
         prefixes.put("US","1");
+        prefixes.put("FR","0");
+        prefixes.put("CHN", "072");
+
 
         NumberParser numberParser = new NumberParser(countryCodes,prefixes);
+
         assertEquals("+442079460056",numberParser.parse("02079460056","+441614960178"));
         assertEquals("+442079460056",numberParser.parse("+442079460056","+441614960178"));
-        assertEquals("+12079460056",numberParser.parse("12079460056","+441614960178"));
+        assertEquals("12079460056",numberParser.parse("12079460056","+441614960178"));
         assertEquals("212079460056",numberParser.parse("212079460056","+441614960178"));
+        assertEquals("+33108822726", numberParser.parse("0108822726", "+33109758351")); //This is the new test scenario
+
+        //extra tests
+        // national to international
+        assertEquals("+442079460056",numberParser.parse("02079460056","+441614960178"));
+        assertEquals("+12079460056",numberParser.parse("12079460056","+11614960178"));
+        assertEquals("+33108822726",numberParser.parse("0108822726","+331614960178"));
+        assertEquals("+862079460056",numberParser.parse("0722079460056","+861614960178"));
+
+        //international to international
+        assertEquals("+12079460056",numberParser.parse("+12079460056","+11614960178"));
+        assertEquals("+441614960178",numberParser.parse("+441614960178","+861614960178"));
+
+        //no national prefix known
+        assertEquals("212079460056",numberParser.parse("212079460056","+441614960178"));
+
+        //international calling code not known
+        assertEquals("212079460056",numberParser.parse("212079460056","+761614960178"));
+
     }
 }
--- src/com/tudor/NumberParser.java
+++ src/com/tudor/NumberParser.java
@@ -1,28 +1,30 @@
 package com.tudor;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * This class returns a dialled number in international format
- * <p>
- * TODO: Class makes assumption that national country codes are unique world wide, in order to associate national codes with international codes
+ * If the dialled number is already in international format , it is simply returned
+ * If the dialled number is in national format, it's prefix is replaced with the international calling code of the user's number
+ * If no national prefix or international calling code is found, the original dialled number is returned. TODO throw exception?
  */
 public class NumberParser {
 
-    private final Map<String, Integer> callingCode;
-    private final Map<String, String> prefixes;
+    private final Map<String, Integer> callingCodesHashMap;
+    private final Map<String, String> prefixesHashMap;
 
     /**
      * Constructor takes
      *
-     * @param callingCode represents mapping between country codes (e.g. "GB", "US") and international dialling code (e.g 44,1)
-     * @param prefixes    represents mapping between country codes (e.g. "GB", "US") and national prefixes (e.g "0","1")
+     * @param callingCodesHashMap represents mapping between country codes (e.g. "GB", "US") and international dialling code (e.g 44,1)
+     * @param prefixesHashMap    represents mapping between country codes (e.g. "GB", "US") and national prefixes (e.g "0","1")
      */
-    public NumberParser(Map<String, Integer> callingCode, Map<String, String> prefixes) {
+    public NumberParser(Map<String, Integer> callingCodesHashMap, Map<String, String> prefixesHashMap) {
 
-        this.callingCode = callingCode;
-        this.prefixes = prefixes;
+        this.callingCodesHashMap = callingCodesHashMap;
+        this.prefixesHashMap = prefixesHashMap;
     }
 
     /**
@@ -31,64 +33,70 @@ public class NumberParser {
      * the method formats the number based on the national prefix
      * and the international dialling code associated with the prefix's country.
      *
-     * @param userNumber has no use in current implementation. TODO: Method makes no use of @param userNumber
+     * @param userNumber has no use in current implementation. 
      */
     public String parse(String dialledNumber, String userNumber) {
 
         if (dialledNumber.substring(0, 1).equals("+")) { // If dialled number starts with "+" character, the number is considered to already be in international format.
             return dialledNumber;
         } else { // dialledNumber needs to be formatted in international format.
-            return formatNationalNumberToInternationalFormat(dialledNumber);
+            return formatNationalNumberToInternationalFormat(dialledNumber,userNumber);
         }
 
     }
 
     /**
      * Method tries to return a national number into it's international format.
-     *
+     * It tries to match all possible international calling codes,
+     * if a match with the dataset is found, it tries to match the original prefix with the know country prefix
      * @param dialledNumber is the number which we wish to format.
+     * @param userNumber the calling user's number.
+     * @return international format of dialledNumber OR original dialledNumber if formatting was not possible
      */
-    private String formatNationalNumberToInternationalFormat(String dialledNumber) {
-        for (int prefixLength = 1; prefixLength < dialledNumber.length(); prefixLength++) {
+    private String formatNationalNumberToInternationalFormat(String dialledNumber,String userNumber) {
 
-            //possible prefix, starting at index 0 and increasing length.
-            String possiblePrefix = dialledNumber.substring(0, prefixLength);
-            int countryCode = nationalToInternationalDiallingCode(possiblePrefix);
-            if (countryCode > 0) {// there exists an associated international code with the possible prefix
-                return buildInternationalFormattedNumber(dialledNumber,prefixLength,countryCode);
+        for (int callingCodeLenght = 1; callingCodeLenght <= 4; callingCodeLenght++) {
+            //possible calling code, starting at index 0 and increasing length.
+            String possibleCallingCode = userNumber.substring(1, callingCodeLenght + 1);// + 1 offsets "+" sign in international number
+            String possibleCountry = findCountryAssociatedWithCallingCode(Integer.valueOf(possibleCallingCode)); // country code associated with possible prefix, empty string if no country found
+            if(!possibleCountry.isEmpty()){
+                    if(nationalNumberBelongsToCountry(possibleCountry,dialledNumber)){//the User's international calling code country matches with the country associated with the dialled number prefix
+                        return buildInternationalFormattedNumber(dialledNumber, prefixesHashMap.get(possibleCountry).length() , callingCodesHashMap.get(possibleCountry));
+                    }
+                }
             }
-        }
-
         //number cannot be formatted
         return dialledNumber;
     }
 
+
     /**
-     * Method returns the associated international dialling code to the given national dialling code.
+    * Method finds a possible country for the given calling code, in the database.
+     * As calling codes are unique, only one match is possible.
      *
-     * @param nationalDiallingCode is the national prefix for which we want the international dialling code.
-     * @return international dialling code OR -1 if no international dialling code is found.
-     */
-    private int nationalToInternationalDiallingCode(String nationalDiallingCode) {
-
-        String absentCountryCode = "NoCty";
-
-        // find country code associated with prefix, if this method is called we are sure the country code exists in the hashmap
-        Optional<Map.Entry<String, String>> optionalCountryCode = prefixes.entrySet().stream()
-                .filter(p -> p.getValue().equals(nationalDiallingCode))
-                .findAny();
-        String countryCode = optionalCountryCode.isPresent() ? optionalCountryCode.get().getKey() : absentCountryCode;
-
-        if (!countryCode.equals(absentCountryCode)) {//there exists an entry with the possible prefix given as parameter
-            Integer callingCodeForPrefix = callingCode.get(countryCode);
-            if (callingCode.get(countryCode) != null) {//there exists and associated international code with the possible prefix given as parameter
-                return callingCodeForPrefix;
-            } else {//there's not international prefix found for the possible prefix.
-                return -1;
-            }
-        }
-        return -1;
+     * @return country code associated with calling code OR empty string, if no match was made
+    * */
+    private String findCountryAssociatedWithCallingCode(Integer possibleCallingCode){
+        return callingCodesHashMap.entrySet().stream()
+                .filter(p -> p.getValue().equals(possibleCallingCode))
+                .map(Map.Entry::getKey)
+                .findFirst().orElse("");
     }
+    /**
+     * Checks whether national number matches to know country prefix
+     *
+     * @param countryCode of country the number might belong to.
+     * @param nationalNumber the number which we want to check.
+     * @return returns TRUE if the nationalNumber belongs to the given country, FALSE otherwise
+     * */
+    private boolean nationalNumberBelongsToCountry(String countryCode, String nationalNumber){
+        String countryPrefix = prefixesHashMap.get(countryCode);
+        if(!countryPrefix.isEmpty()){
+            return countryPrefix.equals(nationalNumber.substring(0,countryPrefix.length()));
+        }
+        return false;
+    }
+
 
     /**
      * Builds the final international format dialling number
@@ -102,7 +110,7 @@ public class NumberParser {
         return stringBuilder
                 .append("+")
                 .append(countryCode)
-                .append(dialledNumber.substring(prefixLength, dialledNumber.length() - prefixLength + 1))
+                .append(dialledNumber.substring(prefixLength))
                 .toString();
 
     }
